05663252f3d0686cc1d2a6591ade946e1cdc5810
3243239283 <treeclass@qq.com>
2016-05-23 17:26:44 +0800

    angular学习16.5.23 17:26

--- js/index.js
+++ js/index.js
@@ -2,20 +2,21 @@
     require('jquery');
     require('angular');
     
-    function HelloAngular($scope){
-		$scope.greeting={
-			text:'Hello'
-		};
-	}
 
 });*/
 
-
-function HelloAngular($scope){
+/*
+备注：
+	*创建全局函数会污染全局命名空间。更合理的方式是创建一个模块(module),然后在模块中创建控制器(controllerName)。
+	*此处创建了一个名为hello的模块,然后在模块中创建了名为HelloAngular的控制器。
+	*页面在使用的时候必须声明ng-app=“模块名”才能调用控制器的方法。
+*/
+var hello=angular.module('hello',[]);
+hello.controller('HelloAngular',function($scope){
 	$scope.greeting={
 		text:'Hello'
 	};
-}
+});
 
 
 
